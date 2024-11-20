661f9ab699e1b3757b950d408e6f97c847735c8b
LAPTOP-AR3EI14D\Robert <c14220241@john.petra.ac.id>
2024-11-20 19:29:34 +0700

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,8 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.jetbrains.kotlin.android) apply false
+
+    //10.03
+    alias(libs.plugins.kotlinParcelize) apply false
+}
